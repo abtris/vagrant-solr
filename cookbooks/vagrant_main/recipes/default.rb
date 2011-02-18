1e3560481e9c82bc0f067dacbcdd7b565a210338
Ladislav Prskavec <ladislav.prskavec@gmail.com>
2011-02-18 16:23:57 +0100

    Add create directory, solr not working yet

--- cookbooks/vagrant_main/recipes/default.rb
+++ cookbooks/vagrant_main/recipes/default.rb
@@ -1,9 +1,18 @@
 require_recipe "capistrano"
 require_recipe "runit"
 require_recipe "java::sun"
-#require_recipe "solr"
+require_recipe "solr::default"
 
 # Some neat package (subversion is needed for "subversion" chef ressource)
 %w{ debconf mc htop curl }.each do |a_package|
   package a_package
 end
+
+
+execute "create-directory" do                                                                                                                                 
+  command "sudo mkdir -p /srv/my_app"                                                                                                                                               
+  action :run                                                                                                                                               
+  ignore_failure true                                                                                                                                       
+end
+
+solr_instance "my_app"
