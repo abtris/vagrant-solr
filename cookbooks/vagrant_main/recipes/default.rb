8507774c7037b2b5f643e7d4c5ae91535a295853
Ladislav Prskavec <ladislav.prskavec@gmail.com>
2011-02-18 15:12:33 +0100

    Update recipes

--- cookbooks/vagrant_main/recipes/default.rb
+++ cookbooks/vagrant_main/recipes/default.rb
@@ -1,4 +1,7 @@
-require_recipe "solr"
+require_recipe "capistrano"
+require_recipe "runit"
+require_recipe "java::sun"
+#require_recipe "solr"
 
 # Some neat package (subversion is needed for "subversion" chef ressource)
 %w{ debconf mc htop curl }.each do |a_package|
